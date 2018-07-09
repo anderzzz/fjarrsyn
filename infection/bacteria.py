8d8bf5951fe808b91eaed2822f7c3f27bd91b5b0
anderzzz <ohrn.anders@gmail.com>
2018-07-08 19:25:01 -0700

    Groundwork for bacterial sensors

--- infection/bacteria.py
+++ infection/bacteria.py
@@ -1,9 +1,9 @@
 '''Bla bla
 
 '''
-from core.agent import Agent
+from core.agent import EngagedAgent
 
-class Bacteria(Agent):
+class Bacteria(EngagedAgent):
     '''Bla bla
 
     '''
@@ -11,20 +11,26 @@ class Bacteria(Agent):
         '''Bla bla
 
         '''
-        return self.nature['surface_profile']
+        return self.scaffold['surface_profile']
+
+    def _execute_probe_surrounding(self) 
 
     def __init__(self, name, surface_profile, molecules):
 
         super().__init__(name)
 
-        self.set_nature('surface_profile', surface_profile)
-        self.set_service('reveal_surface_profile', self._request_surface_profile)
-        self.set_nature('molecule_A', molecules[0])
-        self.set_nature('molecule_B', molecules[1])
-        self.set_nature('molecule_C', molecules[2])
-        self.set_nature('molecule_D', molecules[3])
+        self.set_data('scaffold', 'surface_profile', surface_profile)
+        self.set_organ('service', 'reveal_surface_profile', self._request_surface_profile)
+        self.set_data('scaffold', 'molecule_A', molecules[0])
+        self.set_data('scaffold', 'molecule_B', molecules[1])
+        self.set_data('scaffold', 'molecule_C', molecules[2])
+        self.set_data('scaffold', 'molecule_D', molecules[3])
 
-class ExtracellEnvironment(Agent):
+#        self.set_organ('plan', 'react_to_neighbour', self._execute_react_neighbour)
+        self.set_organ('plan', 'probe_surrounding', self._execute_probe_surrounding)
+#        self.set_organ('plan', 'split_off_child', self._execute_split_child)
+
+class ExtracellEnvironment(PassiveAgent):
     '''Bla bla
 
     '''
@@ -33,4 +39,4 @@ class ExtracellEnvironment(Agent):
         super().__init__(name)
 
         for nature_key, nature_value in natures_init.items():
-            self.set_nature(nature_key, nature_value)
+            self.set_data('scaffold', nature_key, nature_value)
--- infection/goo.py
+++ infection/goo.py
@@ -29,6 +29,22 @@ class Goo(AgentManagementSystem):
         '''
         pass
 
+    def _obtain_bacteria_neighbours(self, agent_index):
+        '''Bla bla
+
+        '''
+        #WORK HERE
+
+    def _add_molecules_to_env(self, agent_index, dx_molecules):
+        '''Bla bla
+
+        '''
+        env_agent = self.agents_in_scope[agent_index]
+        for molecule, dx in dx_molecules.items():
+            x = env_agent.scaffold[molecule]
+            x_new =+ dx
+            env_agent.set_data('scaffold', molecule, x_new)
+
     def __init__(self, name, beaker_length, bacterial_agents, env_agent):
 
         matrix = CubicGrid(n_slots=beaker_length) 
@@ -47,9 +63,9 @@ class Goo(AgentManagementSystem):
                 matrix.nodes[ind].content = {'bacteria':None,
                                              'env':env_agent}
 
-        # WORK HERE
         for bacteria in bacterial_agents:
-            bacteria.set_actuator('take_nutrients', self._take_nutrients)
+            bacteria.set_organ('actuator', 'add_molecules_to_env',
+                                           self._add_molecules_to_env)
 
         agents = bacterial_agents + env_agents
         super().__init__(name, agents, matrix)
