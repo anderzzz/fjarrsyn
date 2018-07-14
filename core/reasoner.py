458ce646b38c58bd126882e6c1ad90b36c0ad9ac
anderzzz <ohrn.anders@gmail.com>
2018-07-13 17:37:29 -0700

    Create basic class outline to handle cognition separate from agent basics

---
+++ core/reasoner.py
@@ -0,0 +1,18 @@
+'''Bla bla
+
+'''
+class Interpreter(object):
+    '''Bla bla
+
+    '''
+    def __init__(self, name):
+    
+        pass
+
+class Handler(object):
+    '''Bla bla
+
+    '''
+    def __init__(self, name):
+
+        pass
---
+++ infection/brain_matter.py
@@ -0,0 +1,12 @@
+'''Bla bla
+
+'''
+from core.reasoner import Interpreter
+
+class PreceptBelief(Interpreter):
+    '''Bla bla
+
+    '''
+    def __init__(self, name):
+
+        pass
