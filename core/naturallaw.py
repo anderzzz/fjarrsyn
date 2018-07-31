53cd7b6cabdad680de43c58b51445ae78367bb16
anderzzz <ohrn.anders@gmail.com>
2018-07-30 21:05:30 -0700

    DOcstrings for the expanded natural laws

--- core/naturallaw.py
+++ core/naturallaw.py
@@ -1,4 +1,5 @@
-'''Bla bla
+'''Methods to perturb or alter in random or deterministic fashion the scaffold
+of an agent or other scaffolded object
 
 '''
 import numpy as np
@@ -10,17 +11,54 @@ ScaffoldForce = namedtuple('ScaffoldForce', ['function', 'kwargs'])
 
 class ObjectForce(object):
     '''Base class for all non-intentional object propagation part of the agent
-    scaffold.
+    or auxiliary object scaffold.
+
+    Parameters
+    ----------
+    name : str
+        Name of the class instance
+    force_scaffold_overlap : bool, optional
+        Boolean flag to check that all force objects for a named scaffold are
+        matched to a scaffold of the given agent. Default set to `False`.
 
     '''
     def force_func_identity(self, old_value):
-        '''Bla bla
+        '''Trivial identity force function. Building-block of composite force
+        functions.
+
+        Parameters
+        ----------
+        old_value 
+            The old_value of the scaffold to update
+
+        Returns
+        -------
+        new_value
+            The new value to update the scaffold to
 
         '''
         return old_value
 
     def _map_force_func(self, force_func):
-        '''Bla bla
+        '''Map input force function argument to a callable. 
+
+        Parameters
+        ----------
+        force_func
+            The input argument for the force function
+
+        Returns
+        -------
+        func : callable
+            The callable function that upon execution creates new value
+
+        Raises
+        ------
+        ValueError
+            If a string given to pre-defined force function, but no such method
+            is defined. Check the string.
+        TypeError
+            If the input is neither string nor a callable
 
         '''
         if isinstance(force_func, str):
@@ -38,20 +76,47 @@ class ObjectForce(object):
         return func
 
     def set_force_func(self, scaffold_name, force_func, force_func_kwargs={}):
-        '''Bla bla
+        '''Associate scaffold with an object force function
+
+        Parameters
+        ----------
+        scaffold_name : str
+            Name of scaffold
+        force_func 
+            String that through mapping points to a pre-defined force function,
+            or a callable that accepts an old value and returns a new value
+            upon execution. The first argument of the callable is mandatory and
+            the old value, additional arguments are provided by kwargs
+            dictionary, see next parameter
+        force_func_kwargs : dict, optional
+            Kwargs dictionary for auxiliary force function arguments
 
         '''
         func = self._map_force_func(force_func)
         self.scaffold_force_func[scaffold_name] = ScaffoldForce(func, force_func_kwargs)
 
     def empty_force_func(self):
-        '''Bla bla
+        '''Eradicates all associations between scaffold and force function.
+        Convenience function for object forces that can only be used once.
 
         '''
         self.scaffold_force_func = {}
 
     def __call__(self, agent):
-        '''Bla bla
+        '''Apply the object forces to the scaffolds of the agent (or other
+        scaffolded object)
+
+        Parameters
+        ----------
+        agent 
+            Instance of the Agent class, or of any other scaffolded class
+
+        Raises
+        ------
+        RuntimeError
+            In case the object force instance is initialized to require that
+            object force scaffolds overlap with agent scaffolds, and an
+            instance of object force is unmatched with the agent scaffold.
 
         '''
         for scaffold_name, force in self.scaffold_force_func.items():
@@ -61,7 +126,11 @@ class ObjectForce(object):
                 new_value = force.function(old_value, **force.kwargs)
                 agent.scaffold[scaffold_name] = new_value
 
-    def __init__(self, name):
+            elif force_scaffold_overlap:
+                raise RuntimeError('The object force for scaffold %s ' %(scaffold_name) + \
+                                   'not matched to any scaffold of agent')
+
+    def __init__(self, name, force_scaffold_overlap=False):
 
         self.name = name 
         self.scaffold_force_func = {}
@@ -69,7 +138,8 @@ class ObjectForce(object):
         self.predefinedforcefunctions = _PreDefinedForceFunctions()
 
 class _PreDefinedForceFunctions(object):
-    '''Bla bla
+    '''Bunch of pre-defined object force functions that other classes can use
+    to associate a callable force function to a scaffold name
 
     '''
     def force_func_delta(self, old_value, increment):
@@ -118,7 +188,16 @@ class _PreDefinedForceFunctions(object):
         raise NotImplementedError('Noisy exponential decay not implemented yet') 
 
 class RandomMutator(ObjectForce):
-    '''Bla bla
+    '''Class to create an object force that randomly mutates a scaffold by some
+    function. This class inherets the `ObjectForce` class.
+
+    Parameters
+    ----------
+    name : str
+        Name of the class instance
+    force_scaffold_overlap : bool, optional
+        Boolean flag to check that all force objects for a named scaffold are
+        matched to a scaffold of the given agent. Default set to `False`.
 
     '''
     def _roll_dice(self, func, thrs):
@@ -156,16 +235,35 @@ class RandomMutator(ObjectForce):
 
         Notes
         -----
-        Replaces the `set_force_func` method of the parent class `ObjectForce`
+        Overrides the `set_force_func` method of the parent class `ObjectForce`,
+        such that random threshold can be applied.
 
-        Bla bla
+        Parameters
+        ----------
+        scaffold_name : str
+            Name of scaffold
+        force_func 
+            String that through mapping points to a pre-defined force function,
+            or a callable that accepts an old value and returns a new value
+            upon execution. The first argument of the callable is mandatory and
+            the old value, additional arguments are provided by kwargs
+            dictionary, see next parameter
+        apply_thrs : float, optional
+            A threshold between 0.0 and 1.0, such that over repeated execution
+            the input force function is applied only with frequence equal to
+            the threshold. In the other cases the force function returns
+            identity and hence leaves the old value unchanged. Visualize this
+            as a proverbial roll of the dice to decide if to apply the object
+            force or not.
+        force_func_kwargs : dict, optional
+            Kwargs dictionary for auxiliary force function arguments
 
         '''
         func = self._map_force_func(force_func)
         func_decorated = self._roll_dice(func, apply_thrs)
         self.scaffold_force_func[scaffold_name] = ScaffoldForce(func_decorated, force_func_kwargs)
 
-    def __init__(self, name):
+    def __init__(self, name, force_scaffold_overlap=False):
 
-        super().__init__(name)
+        super().__init__(name, force_scaffold_overlap)
 
