1fe34d790fda59e17c8d14cf096b8f1691ffe83f
anderzzz <ohrn.anders@gmail.com>
2018-11-13 08:51:46 -0800

    Split curse of the commons into meaningful files

---
+++ systems/curse_commons/ams.py
@@ -0,0 +1,120 @@
+'''Basic Curse of the Commons
+
+'''
+import numpy as np
+import numpy.random
+
+from core.agent import Agent
+from core.agent_ms import AgentManagementSystem
+from core.instructor import Sensor, Interpreter, Moulder, Actuator, Compulsion
+from core.message import Belief, Direction, Resource, Essence
+from core.scaffold_map import ResourceMap, MapCollection
+from core.policy import Clause, Heartbeat, AutoBeliefCondition, \
+                        AutoResourceCondition
+
+class Lake(object):
+
+    def regrowth(self):
+
+        n_spawn = 0
+        for fish in range(0, self.n_fish):
+            n_attempt = int(np.random.ranf() + self.spawn_prob)
+            n_crowding = int(np.random.ranf() + self.n_fish / self.capacity)
+            if n_attempt == 1 and n_crowding == 0:
+                n_spawn += 1
+
+        self.n_fish = min(self.capacity, self.n_fish + n_spawn)
+
+    def extract(self, maximum, extract_prob):
+
+        which_unit_is_fish = np.random.randint(0, self.size_units, self.n_fish)
+        select_unit = np.random.randint(0, self.size_units)
+        max_available = sum([1 for x in which_unit_is_fish if x == select_unit])
+        extract_attempt = [np.random.ranf() < extract_prob for k in range(max_available)]
+        n_extracted = min(maximum, sum(extract_attempt))
+
+        self.n_fish -= n_extracted
+        
+        return n_extracted
+
+    def __init__(self, n_fish, size, spawn_prob, max_capacity):
+
+        self.n_fish = n_fish
+        self.spawn_prob = spawn_prob
+        self.size_units = size
+        self.capacity = max_capacity
+
+BIRTH_PROB = 0.10
+BIRTH_PROB_CAUTION = BIRTH_PROB * 0.5
+STARVE_PROB = 0.25
+
+class World(AgentManagementSystem):
+
+    def eat_life_die(self, people_current, fish_current):
+
+        if fish_current <= people_current:
+            n_starving = people_current - fish_current
+
+            starve_test = [np.random.ranf() < STARVE_PROB for x in range(n_starving)]
+            breed_test = [np.random.ranf() < BIRTH_PROB_CAUTION for x in range(fish_current)]
+
+            delta_people = -1 * sum(starve_test) + sum(breed_test)
+            delta_fish = -1 * fish_current
+
+        elif fish_current >= people_current * 2:
+            breed_test = [np.random.ranf() < BIRTH_PROB for x in range(people_current)]
+            delta_people = sum(breed_test)
+            delta_fish = -2 * people_current
+
+        else:
+            n_full = fish_current - people_current
+            breed_test_1 = [np.random.ranf() < BIRTH_PROB for x in range(n_full)]
+            breed_test_2 = [np.random.ranf() < BIRTH_PROB_CAUTION for x in range(people_current - n_full)]
+            delta_people = sum(breed_test_1) + sum(breed_test_2)
+            delta_fish = -1 * fish_current
+
+        print (fish_current, delta_fish, people_current, delta_people)
+
+        return delta_fish, delta_people
+
+    def extract_from_lake(self, n_boats, max_extract, agent_index):
+
+        if n_boats == 0:
+            return 0
+
+        n_total = 0
+        effort_count = 0
+        while n_total < max_extract:
+            
+            n_fishes = self.common_env.extract(max_extract, 0.5)
+            n_total += n_fishes
+            effort_count += 1
+
+            if effort_count > n_boats:
+                break
+
+        print (n_total)
+        return n_total
+
+    def __init__(self, name, agents, lake):
+
+        super().__init__(name, agents)
+        self.common_env = lake
+
+        map_fish = ResourceMap('fish stock change', 'n_fishes', 'delta', ('adjust_fish',))
+        map_people = ResourceMap('birth death', 'n_people', 'delta', ('adjust_people',))
+        eat_love_death = ResourceMapCollection([map_fish, map_people])
+
+        natural_reqs = Compulsion('survival demands', ['n_people', 'n_fishes'], 
+                                  self.eat_life_die, eat_love_death)
+        self.set_law(natural_reqs)
+
+        for agent in agents:
+            more_fish = ResourceMap('add_fish', 'n_fishes', 'delta', ('add_fishes',))
+            fish_from_lake = Actuator('fish from lake', 
+                                      agent.direction['go fish like this'],
+                                      self.extract_from_lake,
+                                      'fish_results',
+                                      more_fish)
+            agent.set_organ(fish_from_lake)
+
--- systems/curse_commons/society.py
+++ systems/curse_commons/society.py
@@ -12,38 +12,6 @@ from core.scaffold_map import ResourceMap, MapCollection
 from core.policy import Clause, Heartbeat, AutoBeliefCondition, \
                         AutoResourceCondition
 
-class Lake(object):
-
-    def regrowth(self):
-
-        n_spawn = 0
-        for fish in range(0, self.n_fish):
-            n_attempt = int(np.random.ranf() + self.spawn_prob)
-            n_crowding = int(np.random.ranf() + self.n_fish / self.capacity)
-            if n_attempt == 1 and n_crowding == 0:
-                n_spawn += 1
-
-        self.n_fish = min(self.capacity, self.n_fish + n_spawn)
-
-    def extract(self, maximum, extract_prob):
-
-        which_unit_is_fish = np.random.randint(0, self.size_units, self.n_fish)
-        select_unit = np.random.randint(0, self.size_units)
-        max_available = sum([1 for x in which_unit_is_fish if x == select_unit])
-        extract_attempt = [np.random.ranf() < extract_prob for k in range(max_available)]
-        n_extracted = min(maximum, sum(extract_attempt))
-
-        self.n_fish -= n_extracted
-        
-        return n_extracted
-
-    def __init__(self, n_fish, size, spawn_prob, max_capacity):
-
-        self.n_fish = n_fish
-        self.spawn_prob = spawn_prob
-        self.size_units = size
-        self.capacity = max_capacity
-
 class Village(Agent):
 
     def inspect_storage(self, dummy):
@@ -127,6 +95,14 @@ class Village(Agent):
                                            lambda x: x > 0)
         heart = Heartbeat('alive', (heart_cond,))
         self.set_policies(heart)
+
+class Institution(Agent):
+    '''Bla bla
+
+    '''
+    def __init__(self, name):
+
+        super().__init__(name)
 
 BIRTH_PROB = 0.10
 BIRTH_PROB_CAUTION = BIRTH_PROB * 0.5
