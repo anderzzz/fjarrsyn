6c86ff896b9decaaf502ba57aec893a7ef8f9629
anderzzz <ohrn.anders@gmail.com>
2019-03-09 11:00:10 -0800

    Add cortex for cooperation with cooperatives

--- systems/infection/runner.py
+++ systems/infection/runner.py
@@ -4,4 +4,8 @@
 from core.simulator import FiniteSystemRunner
 from core.sampler import AgentSampler, EnvSampler, SystemIO
 
+from unit import Unit
 
+u1 = Unit('test', 0.5, 0.8, 0.0, 1.0, 0.0, 0.0)
+xx = u1.tickle('Reveal Cooperation')
+print (xx)
--- systems/infection/unit.py
+++ systems/infection/unit.py
@@ -4,7 +4,11 @@
 from core.agent import Agent
 
 from core.instructor import Cortex
-from core.message import Essence, Resource, Belief
+from core.message import Essence, Resource, Belief, Feature, MessageOperator
+
+from core.helper_funcs import sigmoid_10
+
+import numpy as np
 
 STRICT_ENGINE = True
 
@@ -12,6 +16,23 @@ class Unit(Agent):
     '''Bla bla
 
     '''
+    def _cmp_frac_share(self, x_val):
+        '''Bla bla
+
+        '''
+        return sigmoid_10(self.essence['max_share'],
+                          self.essence['midpoint_share'],
+                          False, x_val)
+
+    def _cmp_cooperative_feature(self, midpoint_share, max_share, truthfulness):
+        '''Compute a surface feature based on cooperative nature of agent
+
+        '''
+        ff = self._cmp_frac_share(1.0)
+        rando = np.random.uniform()
+        ff = truthfulness * ff + (1.0 - truthfulness) * rando
+
+        return ff
 
     def __init__(self, name, 
                  midpoint_share, max_share,
@@ -30,6 +51,8 @@ class Unit(Agent):
                                  midpoint_gulp, max_gulp,
                                  midpoint_tox, max_tox])
         self.set_scaffold(unit_essence)
+        slicer_of_essence = MessageOperator(unit_essence, 
+                               slice_labels=['midpoint_share', 'max_share'])
 
         #
         # Resource
@@ -41,9 +64,21 @@ class Unit(Agent):
 
         #
         # Belief
-        unit_belief = Belief('Surrounding', ('friendliness',))
+        unit_belief = Belief('Surrounding', ('cooperative_env',))
         unit_belief.set_values([1.0])
         self.set_message(unit_belief)
 
         #
-        # Sensor
+        # Interpreters
+
+        #
+        # Moulders
+
+        #
+        # Cortex
+        coop_expose = Feature('Cooperative Reveal',
+                              ('coop_with_coop',))
+        cortex = Cortex('Reveal Cooperation', self._cmp_cooperative_feature,
+                        slicer_of_essence, coop_expose,
+                        cortex_func_kwargs={'truthfulness' : 1.0})
+        self.set_organ(cortex)
