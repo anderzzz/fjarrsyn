7c1715494cb8525aa0c53cb9293537ebd1cff1d6
anderzzz <ohrn.anders@gmail.com>
2019-03-20 13:13:34 -0700

    Compute phenotype in analysis script

--- systems/infection/analyzer.py
+++ systems/infection/analyzer.py
@@ -5,6 +5,8 @@ import sys
 import os
 import pandas as pd
 
+from core.helper_funcs import sigmoid_10
+
 AGENT_PREFIX = 'agent_sample'
 GRAPH_PREFIX = 'graph_sample'
 ENV_PREFIX = 'env_sample'
@@ -12,7 +14,7 @@ ENV_PREFIX = 'env_sample'
 def get_files(root_path, file_prefix):
 
     uu = os.listdir(root_path)
-    return [f for f in uu if file_prefix in f]
+    return [root_path + '/' + f for f in uu if file_prefix in f]
 
 def get_sample_data(file_paths):
 
@@ -27,19 +29,33 @@ def get_sample_data(file_paths):
 
 def classify_agent(df):
 
-    g_agent = df.groupby(['generation','agent_index'])
-    for key, tt in g_agent:
-        print (tt)
-        raise RuntimeError
-        for row in tt.iterrows():
-            if 'essence' in row[1].loc['variable']:
-                print (row[1])
+    def _pheno(x, p_type, shape, belief):
+        max_val = x['value','essence:Exterior Disposition:max_' + p_type]
+        mid_val = x['value','essence:Exterior Disposition:midpoint_' + p_type]
+        return sigmoid_10(max_val, mid_val, shape, belief)
+
+    df['value','pheno:share_friend'] = df.apply(_pheno, axis=1, 
+                                   belief=1.0, shape=False, p_type='share')
+    df['value','pheno:share_ufriend'] = df.apply(_pheno, axis=1, 
+                                   belief=0.0, shape=False, p_type='share')
+    df['value','pheno:gulp_friend'] = df.apply(_pheno, axis=1, 
+                                   belief=1.0, shape=False, p_type='gulp')
+    df['value','pheno:gulp_ufriend'] = df.apply(_pheno, axis=1, 
+                                   belief=0.0, shape=False, p_type='gulp')
+    df['value','pheno:tox_friend'] = df.apply(_pheno, axis=1, 
+                                   belief=1.0, shape=True, p_type='tox')
+    df['value','pheno:tox_ufriend'] = df.apply(_pheno, axis=1, 
+                                   belief=0.0, shape=True, p_type='tox')
+
+    return df
 
 def main(args):
 
     files_agent_sample = get_files(args[0], AGENT_PREFIX)
 
     df = get_sample_data(files_agent_sample)
+    df = df.set_index(['agent_index','generation','name','variable'])
+    df = df.unstack()
     df = classify_agent(df)
 
 if __name__ == '__main__':
