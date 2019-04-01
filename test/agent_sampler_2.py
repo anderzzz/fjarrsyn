a1dbb37153c951b8fca0edab8bbdb7da96fc28ed
anderzzz <ohrn.anders@gmail.com>
2019-04-01 10:29:28 -0700

    Complete integration test for sampling as agent and AMS method

--- test/agent_sampler_2.py
+++ test/agent_sampler_2.py
@@ -1,13 +1,18 @@
-'''Test of restartable AMS
+'''Test of sampling as method of agent and AMS 
 
 '''
 from core.agent import Agent
 from core.message import Belief, Essence, Resource
 from core.agent_ms import AgentManagementSystem
 from core.graph import Node
-from core.sampler import AgentSampler
+from core.sampler import AgentSampler, EnvSampler, GraphSampler
 
 import networkx as nx
+import pandas as pd
+
+def _get_data(aux_env):
+    return {'env_data_1' : aux_env.env_1, 
+            'env_data_2' : aux_env.env_2}
 
 class Env(object):
 
@@ -56,13 +61,88 @@ a_graph = nx.Graph()
 a_graph.add_edge(n1, n2)
 a_graph.add_edge(n2, n3)
 
+env_sampler = EnvSampler('feel_the_env', _get_data)
+g_sampler = GraphSampler('connections')
+
 mess = AgentManagementSystem('3 bacteria', [a1, a2, a3], a_graph)
+mess.set_sampler(a1.sampler['full_state'])
+mess.set_sampler(env_sampler)
+mess.set_sampler(g_sampler)
 
-xx = a1.sample('full_state')
-print (xx)
-xx = a2.sample('full_state')
-print (xx)
-xx = a3.sample('full_state')
-print (xx)
+x1 = a1.sample('full_state')
+x2 = a2.sample('full_state')
+x3 = a3.sample('full_state')
 
-raise NotImplementedError('Test not fully made')
+y1 = mess.sample('full_state')
+y2 = mess.sample('feel_the_env')
+y3 = mess.sample('connections')
+
+assert (x1.shape == (6, 1))
+assert (x2.shape == (6, 1))
+assert (x3.shape == (6, 1))
+assert (x1.index.nlevels == 4)
+assert (x2.index.nlevels == 4)
+assert (x3.index.nlevels == 4)
+v1 = x1.loc[(slice(None), slice(None), slice(None), 'belief:Bacteria Belief:B1'), 'value'].values[0]
+v2 = x1.loc[(slice(None), slice(None), slice(None), 'belief:Bacteria Belief:B2'), 'value'].values[0]
+v3 = x1.loc[(slice(None), slice(None), slice(None), 'essence:Bacteria Essence:E1'), 'value'].values[0]
+v4 = x1.loc[(slice(None), slice(None), slice(None), 'essence:Bacteria Essence:E2'), 'value'].values[0]
+v5 = x1.loc[(slice(None), slice(None), slice(None), 'resource:Bacteria Resource:R1'), 'value'].values[0]
+v6 = x1.loc[(slice(None), slice(None), slice(None), 'resource:Bacteria Resource:R2'), 'value'].values[0]
+assert (v1 == 0.5)
+assert (v2 == 0.5)
+assert (v3 == 1.0)
+assert (v4 == 1.0)
+assert (v5 == 10.0)
+assert (v6 == 10.0)
+v1 = x2.loc[(slice(None), slice(None), slice(None), 'belief:Bacteria Belief:B1'), 'value'].values[0]
+v2 = x2.loc[(slice(None), slice(None), slice(None), 'belief:Bacteria Belief:B2'), 'value'].values[0]
+v3 = x2.loc[(slice(None), slice(None), slice(None), 'essence:Bacteria Essence:E1'), 'value'].values[0]
+v4 = x2.loc[(slice(None), slice(None), slice(None), 'essence:Bacteria Essence:E2'), 'value'].values[0]
+v5 = x2.loc[(slice(None), slice(None), slice(None), 'resource:Bacteria Resource:R1'), 'value'].values[0]
+v6 = x2.loc[(slice(None), slice(None), slice(None), 'resource:Bacteria Resource:R2'), 'value'].values[0]
+assert (v1 == 0.5)
+assert (v2 == 0.5)
+assert (v3 == 1.0)
+assert (v4 == 0.5)
+assert (v5 == 5.0)
+assert (v6 == 6.0)
+v1 = x3.loc[(slice(None), slice(None), slice(None), 'belief:Bacteria Belief:B1'), 'value'].values[0]
+v2 = x3.loc[(slice(None), slice(None), slice(None), 'belief:Bacteria Belief:B2'), 'value'].values[0]
+v3 = x3.loc[(slice(None), slice(None), slice(None), 'essence:Bacteria Essence:E1'), 'value'].values[0]
+v4 = x3.loc[(slice(None), slice(None), slice(None), 'essence:Bacteria Essence:E2'), 'value'].values[0]
+v5 = x3.loc[(slice(None), slice(None), slice(None), 'resource:Bacteria Resource:R1'), 'value'].values[0]
+v6 = x3.loc[(slice(None), slice(None), slice(None), 'resource:Bacteria Resource:R2'), 'value'].values[0]
+assert (v1 == 0.9)
+assert (v2 == 0.1)
+assert (v3 == 1.1)
+assert (v4 == 0.3)
+assert (v5 == 10.0)
+assert (v6 == 2.0)
+
+assert (y1.shape == (18, 1))
+assert (y1.index.nlevels == 4)
+assert (y1.index.levshape == (1, 3, 3, 6))
+ident = y1 == pd.concat([x1, x2, x3])
+assert (all(ident.values))
+
+assert (y2.shape == (6, 1))
+assert (y2.index.levshape == (1, 3, 3, 2))
+v1 = y2.loc[(slice(None), 'bacteria 1', slice(None), 'env_data_1'), 'value'].values[0]
+v2 = y2.loc[(slice(None), 'bacteria 1', slice(None), 'env_data_2'), 'value'].values[0]
+v3 = y2.loc[(slice(None), 'bacteria 2', slice(None), 'env_data_1'), 'value'].values[0]
+v4 = y2.loc[(slice(None), 'bacteria 2', slice(None), 'env_data_2'), 'value'].values[0]
+v5 = y2.loc[(slice(None), 'bacteria 3', slice(None), 'env_data_1'), 'value'].values[0]
+v6 = y2.loc[(slice(None), 'bacteria 3', slice(None), 'env_data_2'), 'value'].values[0]
+assert (v1 == 0.1)
+assert (v2 == 0.1)
+assert (v3 == 0.2)
+assert (v4 == 0.2)
+assert (v5 == 0.3)
+assert (v6 == 0.3)
+
+REF = [sorted((a1.agent_id_system, a2.agent_id_system)), 
+       sorted((a2.agent_id_system, a3.agent_id_system))]
+for ee in y3.edges:
+    canonical = sorted(ee)
+    assert (canonical in REF)
