a67d841c5ce53508384556d77868c8ef58754a77
anderzzz <ohrn.anders@gmail.com>
2019-10-09 13:44:29 -0700

    Move around some code for better organization

--- fjarrsyn/core/policy.py
+++ fjarrsyn/core/policy.py
@@ -6,92 +6,6 @@ intentional actions
 import networkx as nx
 from collections import Iterable
 
-class Clause(object):
-    '''Collection of multiple atomic verbs that can be executed in defined
-    sequence under a meaningful semantic label. To the clause a binary test of
-    a condition can be attached.
-
-    Parameters
-    ----------
-    name : str
-        The name of the clause
-    verb_phrase : list, optional
-        A list of two-membered tuples. Each tuple defines the atomic verb and
-        the associated phrase, see details in Notes. The order of the tuples
-        defines the order the atomic verbs are executed
-    condition : child of _AutoCondition, optional
-        A child class of the _AutoCondition parent class, which defines a
-        condition to evaluate with respect to an agent belief or resource after
-        the execution of the atomic verbs. 
-
-    Notes
-    -----
-    The clause is defined by a sequence of atomic verb-phrase pairs as well as
-    a condition. A clause can be comprised of one of the two components as
-    well. The verb-phrase pairs are semantically defined, such as
-
-    [('sense', 'noise in surrounding'),('interpret', 'possible ongoing activity')]
-
-    where the first member of each tuple must correspond to an atomic verb of
-    the agent, and the second member of each tuple must correspond to a
-    particular Sensor or Interpreter (in the above example) of the agent. Other
-    verbs and organs can be employed, and longer sequences can be used.
-
-    If only a condition should be checked, the verb-phrase sequence should be
-    left unspecified.
-
-    '''
-    def __call__(self, agent):
-        '''The execution (that is 'prouncement') of the clause for a given
-        agent
-
-        Parameters
-        ----------
-        agent : Agent
-            The agent for whom the clause is executed
-
-        Returns
-        -------
-        truth_value : bool
-            The truth value of the clause. If the clause contains an
-            autocondition, the truth value is the return value of the
-            autocondition. If the clause contains no autocondition, the truth
-            value is the logical conjunction of the return values of the atomic
-            verbs, which should be `True`, unless at least one instructor
-            engine failed to execute witout exception.
-
-        '''
-        ret = True
-        for verb, phrase in self.verb_phrase:
-            ret_tmp = getattr(agent, verb)(phrase)
-            ret = ret and ret_tmp
-
-        truth_value = ret
-
-        if not self.condition is None:
-            truth_value = self.condition(agent, **self.condition_kwargs)
-
-        return truth_value
-
-    def __init__(self, name, verb_phrase=[], condition=None, condition_kwargs={}):
-
-        self.name = name
-
-        if not condition is None:
-            if not isinstance(condition, _AutoCondition): 
-                raise TypeError('The condition of a clause must be an auto condition')
-        self.condition = condition
-        self.condition_kwargs = condition_kwargs
-
-        if not isinstance(verb_phrase, Iterable):
-            raise TypeError('The verb phrase pairs must be part of an iterable')
-        if len(verb_phrase) > 0:
-            for vp in verb_phrase:
-                if len(vp) != 2:
-                    raise ValueError('Each verb phrase entry should be a pair ' + \
-                                     'of strings, the verb, then the phrase')
-        self.verb_phrase = verb_phrase 
-
 class _AutoCondition(object):
     '''Parent class for automatic condition evaluation of a persistent state of
     an Agent
@@ -246,6 +160,189 @@ class AutoResourceCondition(_AutoCondition):
 
         super().__init__(resource_cond_name, cond_func, resource_keys, 
                          cond_func_kwargs)
+
+class Clause(object):
+    '''Collection of multiple atomic verbs that can be executed in defined
+    sequence under a meaningful semantic label. To the clause a binary test of
+    a condition can be attached.
+
+    Parameters
+    ----------
+    name : str
+        The name of the clause
+    verb_phrase : list, optional
+        A list of two-membered tuples. Each tuple defines the atomic verb and
+        the associated phrase, see details in Notes. The order of the tuples
+        defines the order the atomic verbs are executed
+    condition : child of _AutoCondition, optional
+        A child class of the _AutoCondition parent class, which defines a
+        condition to evaluate with respect to an agent belief or resource after
+        the execution of the atomic verbs. 
+
+    Notes
+    -----
+    The clause is defined by a sequence of atomic verb-phrase pairs as well as
+    a condition. A clause can be comprised of one of the two components as
+    well. The verb-phrase pairs are semantically defined, such as
+
+    [('sense', 'noise in surrounding'),('interpret', 'possible ongoing activity')]
+
+    where the first member of each tuple must correspond to an atomic verb of
+    the agent, and the second member of each tuple must correspond to a
+    particular Sensor or Interpreter (in the above example) of the agent. Other
+    verbs and organs can be employed, and longer sequences can be used.
+
+    If only a condition should be checked, the verb-phrase sequence should be
+    left unspecified.
+
+    '''
+    def __call__(self, agent):
+        '''The execution (that is 'prouncement') of the clause for a given
+        agent
+
+        Parameters
+        ----------
+        agent : Agent
+            The agent for whom the clause is executed
+
+        Returns
+        -------
+        truth_value : bool
+            The truth value of the clause. If the clause contains an
+            autocondition, the truth value is the return value of the
+            autocondition. If the clause contains no autocondition, the truth
+            value is the logical conjunction of the return values of the atomic
+            verbs, which should be `True`, unless at least one instructor
+            engine failed to execute witout exception.
+
+        '''
+        ret = True
+        for verb, phrase in self.verb_phrase:
+            ret_tmp = getattr(agent, verb)(phrase)
+            ret = ret and ret_tmp
+
+        truth_value = ret
+
+        if not self.condition is None:
+            truth_value = self.condition(agent, **self.condition_kwargs)
+
+        return truth_value
+
+    def __init__(self, name, verb_phrase=[], condition=None, condition_kwargs={}):
+
+        self.name = name
+
+        if not condition is None:
+            if not isinstance(condition, _AutoCondition): 
+                raise TypeError('The condition of a clause must be an auto condition')
+        self.condition = condition
+        self.condition_kwargs = condition_kwargs
+
+        if not isinstance(verb_phrase, Iterable):
+            raise TypeError('The verb phrase pairs must be part of an iterable')
+        if len(verb_phrase) > 0:
+            for vp in verb_phrase:
+                if len(vp) != 2:
+                    raise ValueError('Each verb phrase entry should be a pair ' + \
+                                     'of strings, the verb, then the phrase')
+        self.verb_phrase = verb_phrase 
+
+
+class Heartbeat(object):
+    '''Basic class the tracks the internal sanity of Agent. 
+
+    Notes
+    -----
+    Non-intentional, yet internal, reasons for loss of function by an agent
+    are tracked by the heart beat class. These are events that make the agent
+    "dead" but not by being killed by an external agent or by intentional
+    decision to kill itself. For example, insufficient internal energy or a
+    terminal age-limit are modelled by heart beat. 
+
+    The heart beat is preferably accessed in an agent via the `pump` verb.
+
+    Parameters
+    ----------
+    name : str
+        Name of particular heart beat 
+    imprint_conditions : Iterable of child class of _AutoCondition, optional
+        A condition on an agent resource or belief that if not satisfied
+        implies loss of function. If none provided, no such conditions exists
+    ticker_arithmetic : callable, optional
+        Function that without any argument returns an integer increment to add
+        to the total number of heart ticks. If none provided, the increment is
+        always one.
+    max_ticker : int, optional
+        An upper limit for the total number of heart ticks of the agent, above
+        which function is lost. If none provided, no such upper limit exists
+
+    Raises
+    ------
+    TypeError
+        If ticker arithmetic or imprint conditions are of wrong type
+
+    '''
+    def __call__(self, agent):
+        '''Perform a heart beat and check any internal conditions for continued
+        survival
+
+        Parameters
+        ----------
+        agent : Agent
+            Agent to check for internal fault conditions and to increase total
+            heart ticker
+
+        Returns
+        -------
+        alive : bool
+            If True, no fault condition is met and the agent remains
+            functional. If False, at least one fault condition has been
+            satisfied and the agent has been made inert
+
+        '''
+        ret = True
+
+        if not self.conditions is None:
+            for condition in self.conditions:
+                if not condition(agent):
+                    agent.inert = True
+                    ret = False
+
+        agent.ticks += self.ticker_arithmetic()
+        if not self.max_ticker is None:
+            if agent.ticks > self.max_ticker:
+                agent.inert = True
+                ret = False
+
+        return ret
+
+    def __init__(self, name, imprint_conditions=None, 
+                 ticker_arithmetic=None, max_ticker=None):
+
+        self.name = name
+
+        if not imprint_conditions is None:
+            if isinstance(imprint_conditions, (AutoResourceCondition,
+                                               AutoBeliefCondition)):
+                self.conditions = [imprint_conditions]
+            else:
+                for condo in imprint_conditions:
+                    if not isinstance(condo, (AutoResourceCondition,
+                                              AutoBeliefCondition)):
+                        raise TypeError('Imprint conditions must be an auto condition')
+                self.conditions = imprint_conditions
+        else:
+            self.conditions = imprint_conditions
+
+        if ticker_arithmetic is None:
+            self.ticker_arithmetic = lambda: 1
+        else:
+            if not callable(ticker_arithmetic):
+                raise TypeError('A non-callable `ticker_arithmetic` encountered')
+            self.ticker_arithmetic = ticker_arithmetic
+
+        self.max_ticker = max_ticker
+
 
 class Plan(object):
     '''Plan that agents can enact as part of their intentional execution. This
@@ -454,99 +551,4 @@ class Plan(object):
             self.tree = tree
 
         self._cargo_counter = 0
-
-class Heartbeat(object):
-    '''Basic class the tracks the internal sanity of Agent. 
-
-    Notes
-    -----
-    Non-intentional, yet internal, reasons for loss of function by an agent
-    are tracked by the heart beat class. These are events that make the agent
-    "dead" but not by being killed by an external agent or by intentional
-    decision to kill itself. For example, insufficient internal energy or a
-    terminal age-limit are modelled by heart beat. 
-
-    The heart beat is preferably accessed in an agent via the `pump` verb.
-
-    Parameters
-    ----------
-    name : str
-        Name of particular heart beat 
-    imprint_conditions : Iterable of child class of _AutoCondition, optional
-        A condition on an agent resource or belief that if not satisfied
-        implies loss of function. If none provided, no such conditions exists
-    ticker_arithmetic : callable, optional
-        Function that without any argument returns an integer increment to add
-        to the total number of heart ticks. If none provided, the increment is
-        always one.
-    max_ticker : int, optional
-        An upper limit for the total number of heart ticks of the agent, above
-        which function is lost. If none provided, no such upper limit exists
-
-    Raises
-    ------
-    TypeError
-        If ticker arithmetic or imprint conditions are of wrong type
-
-    '''
-    def __call__(self, agent):
-        '''Perform a heart beat and check any internal conditions for continued
-        survival
-
-        Parameters
-        ----------
-        agent : Agent
-            Agent to check for internal fault conditions and to increase total
-            heart ticker
-
-        Returns
-        -------
-        alive : bool
-            If True, no fault condition is met and the agent remains
-            functional. If False, at least one fault condition has been
-            satisfied and the agent has been made inert
-
-        '''
-        ret = True
-
-        if not self.conditions is None:
-            for condition in self.conditions:
-                if not condition(agent):
-                    agent.inert = True
-                    ret = False
-
-        agent.ticks += self.ticker_arithmetic()
-        if not self.max_ticker is None:
-            if agent.ticks > self.max_ticker:
-                agent.inert = True
-                ret = False
-
-        return ret
-
-    def __init__(self, name, imprint_conditions=None, 
-                 ticker_arithmetic=None, max_ticker=None):
-
-        self.name = name
-
-        if not imprint_conditions is None:
-            if isinstance(imprint_conditions, (AutoResourceCondition,
-                                               AutoBeliefCondition)):
-                self.conditions = [imprint_conditions]
-            else:
-                for condo in imprint_conditions:
-                    if not isinstance(condo, (AutoResourceCondition,
-                                              AutoBeliefCondition)):
-                        raise TypeError('Imprint conditions must be an auto condition')
-                self.conditions = imprint_conditions
-        else:
-            self.conditions = imprint_conditions
-
-        if ticker_arithmetic is None:
-            self.ticker_arithmetic = lambda: 1
-        else:
-            if not callable(ticker_arithmetic):
-                raise TypeError('A non-callable `ticker_arithmetic` encountered')
-            self.ticker_arithmetic = ticker_arithmetic
-
-        self.max_ticker = max_ticker
 
